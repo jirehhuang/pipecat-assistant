993c1f19edd84212b9172697c2c9a12bb9abacc0
jirehhuang <jirehhuang@ucla.edu>
2025-10-25 21:29:38 -0700

    chore: specify allowed dev origin for client

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    allowedDevOrigins: [process.env.ALLOWED_DEV_ORIGIN || ""]
+};
 
 export default nextConfig;
